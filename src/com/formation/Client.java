4a91f366dafc5752bd3930b146aaa4eb2320d4b6
perecastors <58650@supinfo.com>
2016-10-05 14:01:58 +0200

    Creation Exo Heritage ( 1er add et commit )

---
+++ src/com/formation/Client.java
@@ -0,0 +1,13 @@
+package com.formation;
+
+/**
+ * Created by Formation on 05/10/2016.
+ */
+public class Client extends Personne {
+
+    private String idClient;
+
+    public Client(String nom, String prenom) {
+        super(nom, prenom);
+    }
+}
---
+++ src/com/formation/Personne.java
@@ -0,0 +1,14 @@
+package com.formation;
+
+/**
+ * Created by Formation on 05/10/2016.
+ */
+public class Personne {
+    private String nom;
+    private String prenom;
+
+    public Personne(String nom,String prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
